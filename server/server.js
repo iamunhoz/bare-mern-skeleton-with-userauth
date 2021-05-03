ea4c575842d5b8c8ef727e66f6dc3975f4a9125d
Ivã Munhoz <iamunhoz@gmail.com>
2021-05-03 10:33:59 -0300

    cleaner code about mongo credentials

--- server/server.js
+++ server/server.js
@@ -3,14 +3,17 @@ import express from 'express'
 import { MongoClient } from 'mongodb'
 import template from './../template.js'
 import devBundle from './devBundle.js'
-import secret from './../.secret.js'
+import mongoCredentials from './../.mongoCredentials.js'
 
 const app = express()
 devBundle.compile(app)
 
 const CURRENT_WORKING_DIR = process.cwd()
 app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
-app.get('/', (req, res) => { res.status(200).send(template()) })
+app.get('/', (req, res) => { 
+    console.log(req)
+    res.status(200).send(template()) 
+})
 
 let port = 3001
 app.listen(port, function onStart(err) {
@@ -19,7 +22,7 @@ app.listen(port, function onStart(err) {
     }
     console.info('Server started on port %s', port)
 })    
-const url = "mongodb+srv://"+secret+"?retryWrites=true&w=majority"
+const url = "mongodb+srv://"+mongoCredentials+"?retryWrites=true&w=majority"
 MongoClient.connect(url, (err, db) => {
     console.log("Connected to mongo atlas server")
     db.close()
