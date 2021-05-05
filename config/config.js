1cb814c420cab8928b3698a59cb07806fafcd33b
Ivã Munhoz <iamunhoz@gmail.com>
2021-05-05 15:28:43 -0300

    UserSchema completed, CPS added to template, comment on config about mongoCredentials

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
-import mongoCredentials from './.mongoCredentials.js'
+//mongoCredentials.js contains private password and is not synced with repo. Has to be recreated upon cloning
+import mongoCredentials from './.mongoCredentials.js' 
 
 const config = {
     env: process.env.NODE_ENV || 'development',
@@ -8,3 +9,17 @@ const config = {
 }
 
 export default config
+
+/*  /config/.mongoCredentials.js
+
+const mongoCredentials = {
+    user: 'name',
+    pass: 'word',
+    uri: 'xxxxx.yyyyyy.mongodb.net',
+    defaultDb: 'db-name'
+}
+
+const wholeStr = `${mongoCredentials.user}:${mongoCredentials.pass}@${mongoCredentials.uri}/${mongoCredentials.defaultDb}`
+
+export default wholeStr
+*/
