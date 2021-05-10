368b20d02ae5ba5672985cd5d93f41f0d16e6bf9
Ivã Munhoz <iamunhoz@gmail.com>
2021-05-10 13:43:28 -0300

    authentication flow logic

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -4,9 +4,9 @@ import authCtrl from '../controllers/auth.controller'
 const router = express.Router()
 
 router.route('/auth/signin')
-    .post(authCtrl.signin)
+	.post(authCtrl.signin)
 
 router.route('/auth/signout')
-    .get(authCtrl.signout)
+	.get(authCtrl.signout)
 
 export default router
